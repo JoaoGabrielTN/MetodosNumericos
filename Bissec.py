45ab6847330e778bd18dcc2fe3831c2ac79f2b84
JoaoGabrielTN <joaogt008@gmail.com>
2023-09-17 23:53:54 -0300

    mudança na bissec e falsapos

--- Bissec.py
+++ Bissec.py
@@ -1,24 +1,30 @@
 import math
-def func(x: float) -> float:
+def f1(x: float) -> float:
     """Returns f(x)"""
     return  x**5 + 2*x**3 + 2*x + 1
 
-def bis(a, b, erro):
-    mid = round((a + b)/2, abs(erro))
-    mod = abs(func(mid))
-    while(mod >= 10**erro):
-        if(func(a) < 0):
-            if(func(mid) < 0):
-                a = round(mid, abs(erro))
-            else: 
-                b = round(mid, abs(erro))
-        else:
-            if(func(mid) > 0):
-                a = round(mid, abs(erro))
-            else: 
-                b = round(mid, abs(erro))
-        mid = round((a+b)/2, abs(erro))
-        mod = abs(func(mid))
-    return mid
+def f2(x: float) -> float:
+    return math.cos(x) + math.log(x)
 
-print(bis(-1, 0, -2))
+def f3(x: float) -> float:
+    return x**3+2*x**2-x+1
+
+def bis(a, b, erro, f):
+    mid = round((a + b)/2, abs(erro))
+    mod = abs(f(mid))
+    while(mod >= 10**erro):
+        if(f(a) < 0):
+            if(f(mid) < 0):
+                a = mid
+            else: 
+                b = mid
+        else:
+            if(f(mid) > 0):
+                a = mid
+            else: 
+                b = mid
+        mid = (a+b)/2
+        mod = abs(f(mid))
+    return round(mid, abs(erro))
+
+print(bis(-3, -2, -4, f3))
